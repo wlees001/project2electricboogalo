4611b6162027a22190777b43d0064a8cc1191360
jens87alpen <jenshoffmann20@gmail.com>
2018-03-09 10:07:51 -0800

    api-routes and config.json

--- api-routes.js
+++ api-routes.js
@@ -1,43 +1,46 @@
 const db = require("./models");
 
 const express = require('express');
-// const router = express.Router();
+const router = express.Router();
 
-module.exports = function (router) {
-	
-	router.get('/', function(req, res) {
-		db.Search.findAll().then( (data) => {
-			res.render('index',{
-				eaten : data
-			});
+module.exports = function (app) {
+	app.get("/", function (req, res) {
+            return res.render("index", {
 		})
-	});
-	  
-    
-	//get playlist from the db
-	router.get('/songs', function(req,res){
-		res.send({type:'GET'});
-	});
-	//add new songs to db
-	router.post('/songs', function(req,res){
-		res.send({type:'POST'});
-	});
-	//update playlist in db
-	router.put('/songs', function(req,res){
-		res.send({type:'PUT'});
-	});
+	});	
+	
+//get playlist from the db
+router.get('/songs', function(req,res){
+	res.send({type:'GET'});
+});
+//add new songs to db
+router.post('/songs', function(req,res){
+	res.send({type:'POST'});
+});
+//update playlist in db
+router.put('/songs', function(req,res){
+	res.send({type:'PUT'});
+});
 
-	// get playlist from the db
-	router.get('/recipe', function(req,res){
-		res.send({type:'GET'});
-	});
-	// add new recipe to db
-	router.post('/recipe', function(req,res){
-		res.send({type:'POST'});
-	});
-	// update recipe in db
-	router.put('/recipe', function(req,res){
-		res.send({type:'PUT'});
-	});
+//delete song from db??
+// router.delete('/songs', function(req,res){
+	// res.send({type:'DELETE'});
+// });
 
+// get playlist from the db
+router.get('/recipe', function(req,res){
+	res.send({type:'GET'});
+});
+// add new recipe to db
+router.post('/recipe', function(req,res){
+	res.send({type:'POST'});
+});
+// update recipe in db
+router.put('/recipe', function(req,res){
+	res.send({type:'PUT'});
+});
+// delete recipe in db??
+// router.delete('/recipe', function(req,res){
+// 	res.send({type:'DELETE'});
+// });
 };
