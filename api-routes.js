d5c87f103e96478141d1544cd91f7482fb993779
Kevin Castro <kcastro.build@gmail.com>
2018-03-09 16:48:20 -0800

    remove redundant handlebars and db call for index

--- api-routes.js
+++ api-routes.js
@@ -4,16 +4,12 @@ const express = require('express');
 // const router = express.Router();
 
 module.exports = function (router) {
-	
+
 	router.get('/', function(req, res) {
-		db.Search.findAll().then( (data) => {
-			res.render('index',{
-				eaten : data
-			});
-		})
+		//serve back the main page
+		res.render('index');
 	});
-	  
-    
+
 	//get playlist from the db
 	router.get('/songs', function(req,res){
 		res.send({type:'GET'});
@@ -21,10 +17,6 @@ module.exports = function (router) {
 	//add new songs to db
 	router.post('/songs', function(req,res){
 		res.send({type:'POST'});
-	});
-	//update playlist in db
-	router.put('/songs', function(req,res){
-		res.send({type:'PUT'});
 	});
 
 	// get playlist from the db
@@ -34,10 +26,6 @@ module.exports = function (router) {
 	// add new recipe to db
 	router.post('/recipe', function(req,res){
 		res.send({type:'POST'});
-	});
-	// update recipe in db
-	router.put('/recipe', function(req,res){
-		res.send({type:'PUT'});
 	});
 
 };
